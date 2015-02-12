32d54eed25eed5543cd057786328a30eab44f1d6
Quan Chau <mqchau@uci.edu>
2015-02-11 21:33:40 -0800

    get story path too

--- python/getHttp.py
+++ python/getHttp.py
@@ -1,9 +1,20 @@
+import re
 import pprint
 import requests
 from bs4 import BeautifulSoup
 import argparse
 
-def get_type_description():
+def save_html(file_name, html_string):
+	with open(file_name + ".html", "w") as f:
+		f.write(html_string.encode('utf8'))
+		
+def get_story_in_type(type_str):
+	r = requests.get('http://myhero.com/directory/page.asp', params={'dir': type_str})
+	soup = BeautifulSoup(r.text)
+	save_html('aids_stories', r.text)
+	return None
+
+def get_story_type_description():
 	r = requests.get('http://myhero.com/directory')
 	soup = BeautifulSoup(r.text)
 	all_essayttl = soup.find_all("essayttl")
@@ -15,6 +26,7 @@ def extract_type_description(x):
 	return {
 		"type": x.find("essayttl").contents[0]
 		,"description": x.contents[-1].strip()
+		,"tag": x['href'] 
 		}
 
 
@@ -33,5 +45,10 @@ if __name__ == "__main__":
 			with open("story_types.html", "w") as f:
 				f.write(r.text.encode('utf8'))
 		elif int(args.debug) == 1:
-			all_type_description = get_type_description()
-			pp.pprint(all_type_description)
+			#get all types of stories and their descriptions 
+			all_story_type_description = get_story_type_description()
+			pp.pprint(all_story_type_description)
+		elif int(args.debug) == 2:
+			#get all stories of a specific type, let's test with aids
+			all_stories = get_story_in_type('aids')
+			pp.pprint(all_stories)
