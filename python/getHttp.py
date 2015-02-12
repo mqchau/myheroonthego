01ca5d409fd990ae3012cdc4756281a5ebe331fa
Quan Chau <mqchau@uci.edu>
2015-02-11 21:10:02 -0800

    rearrange code a bit

--- python/getHttp.py
+++ python/getHttp.py
@@ -3,6 +3,14 @@ import requests
 from bs4 import BeautifulSoup
 import argparse
 
+def get_type_description():
+	r = requests.get('http://myhero.com/directory')
+	soup = BeautifulSoup(r.text)
+	all_essayttl = soup.find_all("essayttl")
+	all_essayttl_parent = map(lambda x: x.parent, all_essayttl)
+	all_essayttl_profile = map(extract_type_description, all_essayttl_parent)
+	return all_essayttl_profile
+
 def extract_type_description(x):
 	return {
 		"type": x.find("essayttl").contents[0]
@@ -25,12 +33,5 @@ if __name__ == "__main__":
 			with open("story_types.html", "w") as f:
 				f.write(r.text.encode('utf8'))
 		elif int(args.debug) == 1:
-			#load the list of stories in html format and parse it
-			with open("story_types.html", "r") as f:
-				html_string = f.read()
-			soup = BeautifulSoup(html_string)
-		#	print (soup.prettify())
-			all_essayttl = soup.find_all("essayttl")
-			all_essayttl_parent = map(lambda x: x.parent, all_essayttl)
-			all_essayttl_profile = map(extract_type_description, all_essayttl_parent)
-			pp.pprint(all_essayttl_profile)
+			all_type_description = get_type_description()
+			pp.pprint(all_type_description)
