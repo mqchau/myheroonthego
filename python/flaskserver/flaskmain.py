4f7f939df8566bfc3eaf7cf979e81750e4cae018
Quan Chau <quanqchau@gmail.com>
2015-02-17 13:54:18 -0800

    flask can give list of story in a category

--- python/flaskserver/flaskmain.py
+++ python/flaskserver/flaskmain.py
@@ -8,8 +8,13 @@ def hello_world():
 
 @app.route('/getAllStory')
 def return_all_story():
-#	print json.dumps(myhero_story.get_story_type_description())
 	return json.dumps(myhero_story.get_story_type_description())
+
+@app.route('/storyList/<story_category>')
+def return_story_in_category(story_category):
+	print 'got ' + story_category
+	print json.dumps(myhero_story.get_story_in_type(story_category))
+	return json.dumps(myhero_story.get_story_in_type(story_category))
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0')
